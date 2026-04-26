abaab480f17d3056bfbe49ac33ad963cd2f36b51
cdumandan <claritodumandanjr@gmail.com>
2026-04-26 18:55:55 +0800

    yey gamay na lng

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 // Set this before deploying the frontend.
 // Example:
 // window.API_BASE_URL = "https://store-system-x54l.onrender.com";
-window.API_BASE_URL = "https://store-system-x54l.onrender.com";
+window.API_BASE_URL = "https://store-system-2amr.onrender.com";
 
